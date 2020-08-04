1628313ae80076a4e3c9792bacfeef233d905873
Kaustav Sharma <kaustav927@gmail.com>
2020-08-04 17:39:16 -0400

    Updates

---
+++ precache-manifest.f1a37bb16dc22f5534e4d30bd6099ba8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79719872f4acf2a7f1d36a5a3e6816ce",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "e4fb084b11f6a37445d6",
+    "url": "/deploy-react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "85cce1f2d8add4e186c0",
+    "url": "/deploy-react/static/css/main.c9f151e1.chunk.css"
+  },
+  {
+    "revision": "e4fb084b11f6a37445d6",
+    "url": "/deploy-react/static/js/2.45b3e566.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/deploy-react/static/js/2.45b3e566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85cce1f2d8add4e186c0",
+    "url": "/deploy-react/static/js/main.8a8a5d7f.chunk.js"
+  },
+  {
+    "revision": "25575d785b34f1bb97a3",
+    "url": "/deploy-react/static/js/runtime-main.ecea5f17.js"
+  },
+  {
+    "revision": "a77ab372f78b0a2711d4ebf2c9e49c20",
+    "url": "/deploy-react/static/media/ChangeX-PW.a77ab372.png"
+  },
+  {
+    "revision": "b960bc8feb430d9473b1e164fdcb8309",
+    "url": "/deploy-react/static/media/Game-PW.b960bc8f.png"
+  },
+  {
+    "revision": "c863f8028c2505f92540e0ba7c379002",
+    "url": "/deploy-react/static/media/PermanentMarker-Regular.c863f802.ttf"
+  },
+  {
+    "revision": "28324e6f141b56321f25a87020e9a4f1",
+    "url": "/deploy-react/static/media/Sent_analysis_PW.28324e6f.png"
+  },
+  {
+    "revision": "159cb67fc3bc762a8c3232f0a0c6728e",
+    "url": "/deploy-react/static/media/VarelaRound-Regular.159cb67f.ttf"
+  },
+  {
+    "revision": "db59449d91a7090e20dd3846e7a464ff",
+    "url": "/deploy-react/static/media/triangles.db59449d.png"
+  }
+]);
