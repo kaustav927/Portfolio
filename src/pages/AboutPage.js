5357186f1b7fc107fea353c240e76bdd66d1a19b
kaustav927 <kaustav927@gmail.com>
2020-10-15 17:15:38 -0400

    skills update

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Content from '../components/Content'
 
+
 import { Container, Card, Row} from 'react-bootstrap'
 import './About.css'
 
@@ -10,23 +11,32 @@ function AboutPage(props){
         <div id="about" className="aboutStyle">
         <Container>
             <Content>
-                <h1>About Me</h1>
-                <p>
-                Hello! My name is Kaustav (kohs-tohv) and I am a third year CS student at Western University (UWO). I love making things. While hard technical problems are intrinsically fun for me to tackle, I  am most intrigued by creating tastefully designed products for customers with an entrepreneurial justification. I am on a quest to find opportunities that will improve myself as a developer, and allow me to collaborate with awesome people who complement my skills. 
-                </p>
-
+            <h1>About Me</h1>
+            
+            
                 <h3 className="subHeadings">Experience</h3>
+
                 <Row className="justify-content-around ">
-                   
-               
+
                 <Card className="experienceCard"  style={{ width: '18rem' }}>
-                <Card.Body className="callUnify_cardStyle">
+                <Card.Body className="job_cardStyle">
                     <Card.Title>callUnify</Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">QA Software Developer</Card.Subtitle>
                     <Card.Text>
-                    Develop unit tests, conduct competitor research, and document user-reported bugs.
+                    Developed unit tests, conducted competitor research, and documented user-reported bugs.
                     </Card.Text>
-                    <Card.Link href="https://callunify.com/">Visit Site</Card.Link>
+                    <Card.Link href="https://callunify.com/">Visit Company Site</Card.Link>
+                </Card.Body>
+                </Card>
+
+                <Card className="experienceCard"  style={{ width: '18rem' }}>
+                <Card.Body className="job_cardStyle">
+                    <Card.Title>Finastra</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Associate Software Engineer Co-op</Card.Subtitle>
+                    <Card.Text>
+                    Collaborate with software archetects, gaining valuable experiences pertaining to microsystem archetecture. 
+                    </Card.Text>
+                    <Card.Link href="https://westerngazette.ca/">Visit Company Site</Card.Link>
                 </Card.Body>
                 </Card>
 
@@ -37,22 +47,51 @@ function AboutPage(props){
                     <Card.Text>
                     Collaborate with storytellers to create well-recived graphical elements that complement stories. 
                     </Card.Text>
-                    <Card.Link href="https://westerngazette.ca/">Visit Site</Card.Link>
+                    <Card.Link href="https://westerngazette.ca/">Visit Company Site</Card.Link>
                 </Card.Body>
                 </Card>
-
                 </Row>
 
-                <h3 className="subHeadings">Skills</h3>
-                <ul>
-                    <li>Python</li>
-                    <li>Javascript</li>
-                    <li>Javascript</li>
-                    <li>C</li>
-                </ul>
 
+              <Container>
+              <Row>
+                <h3 className="subHeadings">Languages</h3>
+                </Row>
+                <Row>
+                    <div className="skills"> Python </div>
+                    <div className="skills"> Javascript </div>
+                    <div className="skills"> Java </div>
+                    <div className="skills"> C++ </div>
+                    <div className="skills"> SQL </div>
+                    <div className="skills"> R </div>
+                </Row>
+
+
+                <Row>
+                <h3 className="subHeadings">Technologies</h3>
+                </Row>
+                <Row>
+                    <div className="skills"> React </div>
+                    <div className="skills"> Node </div>
+                    <div className="skills"> Express </div>
+                    <div className="skills"> MongoDB </div>
+                    <div className="skills"> AWS </div>
+                    <div className="skills"> Spring Boot </div>
+                    <div className="skills"> Linux </div>
+                    <div className="skills"> Git </div>
+                </Row>
+              </Container>
+                
+
+              
+                
+
+               
+                
+             
 
             </Content>
+            
             </Container>
         </div>
     );
